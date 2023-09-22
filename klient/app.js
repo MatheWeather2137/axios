fa7144938c3c7185b9cbf322303708f8d37d6b79
MatheWeather2137 <126457939+MatheWeather2137@users.noreply.github.com>
2023-09-22 16:30:25 +0200

    .

--- klient/app.js
+++ klient/app.js
@@ -1,19 +1,30 @@
+const menu = document.getElementById("menu")
+const content = document.getElementById("content")
+const aha = document.getElementById("checkboxes")
 
 async function getData(){
-    const data = await fetch("http://192.168.15.4/kontynent")
+    const data = await fetch("http://localhost:3001/kon")
     const json = await data.json()
     console.log(json)
 
-    document.getElementById("menu").innerHTML = ""
-
     for(let i=0; i>=json.length-1; i++){
         const div = document.createElement("div")
-        div.classList.add("kont")
-
+        div.setAttribute("id","divv")
+        menu.appendChild(div)
+        
         const checkbox = document.createElement("input")
         checkbox.setAttribute("type","checkbox")
-        div.appendChild("checkbox")
+        checkbox.classList.add("input_checkbox")
+        checkbox.setAttribute("name","continent")
+        checkbox.setAttribute("value",`${json[i].continent}`)
+        checkbox.setAttribute("id",`continent${i}`)
         
+        const label = document.createElement("label")
+        label.setAttribute("for",`continent${i}`)
+        label.innerHTML = document.getElementById(`continent${i}`).value
+
+        div.appendChild(checkbox)
+        div.appendChild(label)        
     }
 }
 getData()
